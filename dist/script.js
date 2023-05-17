f07d9af92b0269eb270d77ab591cd78f54126bcf
malweis <58945171+malweis@users.noreply.github.com>
2023-05-17 09:16:53 -0400

    feat: Añade funcionalidad abrir detalle
    los botones de abrir detalle funcionan apropiadamente

--- dist/script.js
+++ dist/script.js
@@ -20,3 +20,26 @@ buttons.forEach(function(button) {
     }
   });
 });
+var abrirDetalleButtons = document.querySelectorAll(".abrir-detalle-button");
+
+abrirDetalleButtons.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+    var liParent = event.target.closest("li").parentElement;
+    var codeBlock = liParent.querySelector(".code-blocks");
+
+    if (codeBlock.style.display === "block") {
+      codeBlock.style.opacity = "0";
+      setTimeout(function() {
+        codeBlock.style.display = "none";
+      }, 500);
+    } else {
+      codeBlock.style.display = "block";
+      setTimeout(function() {
+        codeBlock.style.opacity = "1";
+      }, 100);
+    }
+  });
+});
+
+
+
