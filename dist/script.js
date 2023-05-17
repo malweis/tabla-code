7a5913e91b75d28085bb9adf4b9fa3a23f5b90cb
malweis <58945171+malweis@users.noreply.github.com>
2023-05-17 10:14:07 -0400

    feat: Añade todos los botones
    Botones funcionales y la transicion ya funciona

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,56 @@
+var buttons = document.querySelectorAll(".ver-archivo-button");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+    var versionDiv = document.getElementById("archivo");
+    var versionName = event.target.closest("tr").querySelector("td:first-child").textContent.trim();
+    var versionTitle = versionDiv.querySelector("h1");
+    var versionVersion = versionDiv.querySelector("h2");
+
+    versionTitle.textContent = "Archivos" ;
+    versionVersion.textContent =  "Version " + versionName ;
+
+    if (versionDiv.style.display === "flex") {
+      versionDiv.style.opacity = "0";
+      setTimeout(function() {
+        versionDiv.style.display = "none";
+      }, 500);
+    } else {
+      versionDiv.style.display = "flex";
+      setTimeout(function() {
+        versionDiv.style.opacity = "1";
+      }, 100);
+    }
+  });
+});
+
+var buttons = document.querySelectorAll(".ver-detalle-button");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+    var versionDiv = document.getElementById("detalle");
+    var versionName = event.target.closest("tr").querySelector("td:first-child").textContent.trim();
+    var versionTitle = versionDiv.querySelector("h1");
+    var versionVersion = versionDiv.querySelector("h2");
+
+    versionTitle.textContent = "Detalles" ;
+    versionVersion.textContent =  "Version " + versionName ;
+
+    if (versionDiv.style.display === "flex") {
+      versionDiv.style.opacity = "0";
+      setTimeout(function() {
+        versionDiv.style.display = "none";
+      }, 500);
+    } else {
+      versionDiv.style.display = "flex";
+      setTimeout(function() {
+        versionDiv.style.opacity = "1";
+      }, 100);
+    }
+  });
+});
+
+
 var buttons = document.querySelectorAll(".ver-config-button");
 
 buttons.forEach(function(button) {
@@ -5,7 +58,10 @@ buttons.forEach(function(button) {
     var versionDiv = document.getElementById("version");
     var versionName = event.target.closest("tr").querySelector("td:first-child").textContent.trim();
     var versionTitle = versionDiv.querySelector("h1");
-    versionTitle.textContent = versionName;
+    var versionVersion = versionDiv.querySelector("h2");
+
+    versionTitle.textContent = "Configuracion" ;
+    versionVersion.textContent =  "Version " + versionName ;
 
     if (versionDiv.style.display === "flex") {
       versionDiv.style.opacity = "0";
